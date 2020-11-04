2c8999f277322b2f6a5ec658ed8c263380404bdc
Niles Rogoff <me@niles.xyz>
2020-11-04 13:51:37 -0800

    include nmap dependency for ncat command line

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-make -C server all vendor/websocketd/websocketd
+make -C server all vendor/websocketd/websocketd vendor/nmap/bin/cat
 server/server &
-server/vendor/websocketd/websocketd --port 8080 --staticdir www ncat 127.0.0.1 9123 &
+server/vendor/websocketd/websocketd --port 8080 --staticdir www vendor/nmap/bin/ncat 127.0.0.1 9123 &
 wait
